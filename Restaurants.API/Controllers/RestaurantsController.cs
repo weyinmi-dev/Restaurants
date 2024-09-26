3c29f2548b501f69061723e9d33d5756a90391fd
weyinmi-dev <weyinmi.e.madamedon@gmail.com>
2024-09-26 01:39:48 +0100

    Add project files.

---
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Restaurants;
+
+namespace Restaurants.API.Controllers;
+
+[ApiController]
+[Route("api/restaurants")]
+public class RestaurantsController(IRestaurantsService restaurantsService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var restaurants = await restaurantsService.GetAllRestaurants();
+        return Ok(restaurants);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById([FromRoute] int id)
+    {
+        var resturants = await restaurantsService.GetRestaurantById(id);
+
+        if (resturants is null)
+            return NotFound();
+
+        return Ok(resturants);
+    }
+}
---
+++ Restaurants.Application/Restaurants/RestaurantsService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
+using System.ComponentModel.DataAnnotations;
+
+namespace Restaurants.Application.Restaurants;
+
+internal class RestaurantsService(IRestaurantsRepository restaurantsRepository,
+    ILogger<RestaurantsService> logger) : IRestaurantsService
+{
+    public async Task<IEnumerable<Restaurant>> GetAllRestaurants()
+    {
+        logger.LogInformation("Getting all restaurants");
+        var restaurants = await restaurantsRepository.GetAllAsync();
+        return restaurants;
+    }
+
+    public async Task<Restaurant?> GetRestaurantById(int id)
+    {
+        logger.LogInformation($"Getting restaurant by {id}");
+        var restaurant = await restaurantsRepository.GetById(id);
+        return restaurant;
+    }
+}
