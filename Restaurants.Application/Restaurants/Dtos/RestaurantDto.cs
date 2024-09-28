0cfba55d23db389dd8a1c183244b6ccf2ed1d47f
weyinmi-dev <weyinmi.e.madamedon@gmail.com>
2024-09-28 02:12:25 +0100

    ResturantDtos and DishDto added

---
+++ Restaurants.Application/Restaurants/Dtos/RestaurantDto.cs
@@ -0,0 +1,24 @@
+﻿using Restaurants.Application.Dishes.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Restaurants.Dtos
+{
+    public class RestaurantDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string Category { get; set; } = default!;
+        public bool HasDelivery { get; set; }
+        public string? City { get; set; }
+        public string? Street { get; set; }
+        public string? PostalCode { get; set;}
+
+        public List<DishDto> Dishes { get; set; } = new();
+
+    }
+}
