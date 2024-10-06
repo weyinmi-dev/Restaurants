ecfae016ca8047ab53ce6434db1cb4551419c217
weyinmi-dev <weyinmi.e.madamedon@gmail.com>
2024-10-06 04:12:56 +0100

    Serilog-updated

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -3,6 +3,7 @@ using Restaurants.Infrastructure.Seeders;
 using Restaurants.Application.Extensions;
 using Serilog;
 using Serilog.Events;
+using Serilog.Formatting.Compact;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,9 +15,7 @@ builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Host.UseSerilog((context, configuration) =>
     configuration
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information) 
-    .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM-YYYY HH:mm:ss} {Level:u3}] |{SourceContext}|{NewLine}{Message:lj}{NewLine}{Exception}")
+    .ReadFrom.Configuration(context.Configuration)
 );
 
 var app = builder.Build();
